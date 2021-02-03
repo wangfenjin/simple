fe7e12e662ae9f03fd4c4eccd6b142d618f0c36c
wangfenjin <wangfenj@gmail.com>
2021-02-03 15:48:00 +0800

    add more tests
    Change-Id: Ia763c19d9b240bfd770c15d914be9b73ea217ba1

--- test/pinyin_test.cc
+++ test/pinyin_test.cc
@@ -8,6 +8,8 @@ TEST(simple, pinyin_split) {
   PinYin* pinyin = new PinYin();
   auto res = pinyin->split_pinyin("a");
   ASSERT_EQ(res.size(), 1);
+  res = pinyin->split_pinyin("ba");
+  ASSERT_EQ(res.size(), 2);
   res = pinyin->split_pinyin("zhangliangy");
   ASSERT_EQ(res.size(), 4);
   for (auto r : res) std::cout << r << "\t";
