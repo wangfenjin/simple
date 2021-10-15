0839ad266121ba4692f40e77fdbc7d621bba54a3
wangfenjin <wangfenj@gmail.com>
2021-10-15 23:43:21 +0800

    fix windows

--- examples/node/just-task.js
+++ examples/node/just-task.js
@@ -6,17 +6,21 @@ task('install', () => {
   return new Promise((resolve, reject) => {
     const localPath = path.join(__dirname, 'lib')
     var platform = process.env.npm_config_target_platform || process.platform
+    logger.info(`[install] Target platform: ${platform}`)
     if (platform === 'darwin') {
       platform = 'osx';
+    } else if (platform === 'win32') {
+      platform = 'windows';
     }
     var arch = process.env.npm_config_target_arch || process.arch
+    logger.info(`[install] Target arch: ${arch}`)
     if (arch === 'x64') {
-      arch = 'amd64'
+      arch = 'amd64';
+    } else if (arch === 'amd64') {
+      arch = 'x64';
     }
     const downloadUrl = `https://github.com/wangfenjin/simple/releases/download/v0.0.4/libsimple-${platform}-${arch}.zip`
     logger.info(`[install] Download prebuilt binaries from ${downloadUrl}`)
-    logger.info(`[install] Target platform: ${platform}`)
-    logger.info(`[install] Target arch: ${arch}`)
     download(downloadUrl, localPath, {
         extract: true, strip: 1
     }).then(() => {
