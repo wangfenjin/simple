64e58b7702b5d7ec91d5349153585dfcca042c7a
wangfenjin <wangfenj@gmail.com>
2021-10-15 22:40:04 +0800

    add jieba_query example

--- examples/node/node-sqlite3.js
+++ examples/node/node-sqlite3.js
@@ -2,11 +2,28 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
 db.serialize(function() {
+    // load extension
     db.loadExtension("./lib/libsimple");
+    // set the jieba dict file path
+    db.run("select jieba_dict('./lib')");
+    // create table
     db.run("CREATE VIRTUAL TABLE t1 USING fts5(x, tokenize = 'simple')");
-    db.run("insert into t1(x) values ('周杰伦 Jay Chou:我已分不清，你是友情还是错过的爱情'), ('周杰伦 Jay Chou:最美的不是下雨天，是曾与你躲过雨的屋檐'), ('I love China! 我爱中国!'), ('@English &special _characters.\"''bacon-&and''-eggs%')");
+    // insert some data
+    db.run("insert into t1(x) values ('周杰伦 Jay Chou:我已分不清，你是友情还是错过的爱情'), ('周杰伦 Jay Chou:最美的不是下雨天，是曾与你躲过雨的屋檐'), ('I love China! 我爱中国！我是中华人民共和国公民！'), ('@English &special _characters.\"''bacon-&and''-eggs%')");
 
+    // with match 周杰伦
+    console.log("simple_query('zjl'):");
     db.each("select rowid as id, simple_highlight(t1, 0, '[', ']') as info from t1 where x match simple_query('zjl')", function(err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+    // will match 中国 and 中华人民共和国
+    console.log("simple_query('中国'):");
+    db.each("select rowid as id, simple_highlight(t1, 0, '[', ']') as info from t1 where x match simple_query('中国')", function(err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+    // will match 中国 but not 中华人民共和国
+    console.log("jieba_query('中国'):");
+    db.each("select rowid as id, simple_highlight(t1, 0, '[', ']') as info from t1 where x match jieba_query('中国')", function(err, row) {
         console.log(row.id + ": " + row.info);
     });
 });
