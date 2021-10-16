cbf5bb6f02cee46de8fe40e8d16695a9106b30c2
wangfenjin <wangfenj@gmail.com>
2021-10-16 19:51:49 +0800

    use build output to run example

--- examples/node/node-sqlite3.js
+++ examples/node/node-sqlite3.js
@@ -2,18 +2,36 @@ var path = require("path");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
+const process = require( 'process' );
+
+const argv = key => {
+    // Return true if the key exists and a value is defined
+    if ( process.argv.includes( `--${ key }` ) ) return true;
+    const value = process.argv.find( element => element.startsWith( `--${ key }=` ) );
+    // Return null if the key does not exist and a value is not defined
+    if ( !value ) return null;
+    return value.replace( `--${ key }=` , '' );
+}
+
 db.serialize(function() {
-    const extension_path = path.resolve("./lib/");
-    console.log("extension path: " + extension_path);
+    var ext_path = path.resolve("./lib/");
+    if (argv('ext_path')) {
+        ext_path = argv('ext_path');
+    }
+    var dict_path = path.join(ext_path, "dict");
+    if (argv('dict_path')) {
+        dict_path = argv('dict_path');
+    }
+    console.log("extension path: " + ext_path + ", dict path: " + dict_path);
     // load extension
     var platform = process.env.npm_config_target_platform || process.platform
     if (platform === 'win32') {
-      db.loadExtension(path.join(extension_path, "simple"));
+      db.loadExtension(path.join(ext_path, "simple"));
     } else {
-      db.loadExtension(path.join(extension_path, "libsimple"));
+      db.loadExtension(path.join(ext_path, "libsimple"));
     }
     // set the jieba dict file path
-    db.run("select jieba_dict(?)", path.join(extension_path, "dict"));
+    db.run("select jieba_dict(?)", dict_path);
     // create table
     db.run("CREATE VIRTUAL TABLE t1 USING fts5(x, tokenize = 'simple')");
     // insert some data
