cf1e255517e82f70c77f1b19b0faac0101db9637
boyan <17426470+boyan01@users.noreply.github.com>
2021-12-08 18:22:41 +0800

    improve ios build

---
+++ build-ios.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+current_dir=$(pwd)/$(dirname "$0")
+build_dir="${current_dir}/build-ios"
+lib_prefix="${current_dir}/output"
+
+cmake "$current_dir" -G Xcode -DCMAKE_TOOLCHAIN_FILE=contrib/ios.toolchain.cmake \
+ -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=1 \
+ -DCMAKE_INSTALL_PREFIX="" -B "$build_dir" \
+ -DDEPLOYMENT_TARGET=8.0
+
+cd "$build_dir" || exit
+
+cmake --build "$build_dir" --config Release
+cmake --install "$build_dir" --config Release --prefix "${lib_prefix}"
