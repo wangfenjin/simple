8625202a4f79798f170a6f6ffa0d67d0199b8e8a
lihengdao <4548212@qq.com>
2022-10-22 23:52:59 +0800

    add node window download url

--- examples/node/just-task.js
+++ examples/node/just-task.js
@@ -1,28 +1,41 @@
-const { task, logger } = require('just-scripts')
-const download = require('download')
-const path = require('path')
-
-task('install', () => {
+const { task, logger } = require("just-scripts");
+const download = require("download");
+const path = require("path");
+task("install", () => {
   return new Promise((resolve, reject) => {
-    const localPath = path.join(__dirname, 'lib')
-    var platform = process.env.npm_config_target_platform || process.platform
-    logger.info(`[install] Target platform: ${platform}`)
-    if (platform === 'darwin') {
-      platform = 'osx';
-    } else if (platform === 'win32') {
-      platform = 'windows';
+    const localPath = path.join(__dirname, "lib");
+    var platform = process.env.npm_config_target_platform || process.platform;
+    var arch = process.env.npm_config_target_arch || process.arch;
+
+    let downloadUrl = `https://github.com/wangfenjin/simple/releases/download/v0.2.0-alpha/libsimple-linux-ubuntu-18.04.zip`;
+    if (platform === "darwin") {
+      platform = "osx";
+    } else if (platform === "win32") {
+      platform = "windows";
+      if (arch === "x64") {
+        downloadUrl = `https://github.com/wangfenjin/simple/releases/download/v0.2.0-alpha/libsimple-windows-x64.zip`;
+      } else {
+        downloadUrl = `https://github.com/wangfenjin/simple/releases/download/v0.2.0-alpha/libsimple-windows-x86.zip`;
+      }
     }
-    var arch = process.env.npm_config_target_arch || process.arch
-    logger.info(`[install] Target arch: ${arch}`)
-    const downloadUrl = `https://github.com/wangfenjin/simple/releases/download/v0.2.0-alpha/libsimple-linux-ubuntu-18.04.zip`
-    logger.info(`[install] Download prebuilt binaries from ${downloadUrl}`)
+
+    logger.info(`[install] Target platform: -${platform}-`);
+    logger.info(`[install] Target arch: ${arch}`);
+    logger.info(`[install] Download prebuilt binaries from ${downloadUrl}`);
+
+    
     download(downloadUrl, localPath, {
-        extract: true, strip: 1
-    }).then(() => {
-      resolve()
-    }).catch(err => {
-      logger.warn(`[install] Failed to download package from: ${downloadUrl}, err: ${err}`)
-      reject()
+      extract: true,
+      strip: 1,
     })
-  })
-})
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        logger.warn(
+          `[install] Failed to download package from: ${downloadUrl}, err: ${err}`
+        );
+        reject();
+      });
+  });
+});
