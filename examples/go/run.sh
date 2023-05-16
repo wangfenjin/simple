d396238e92b7060952a5e6e6fc1bc524b3990d1e
Halimao <1065621723@qq.com>
2023-05-16 18:01:19 +0800

    Update run.sh

--- examples/go/run.sh
+++ examples/go/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-wget -qO- https://github.com/wangfenjin/simple/releases/download/v0.1.0/libsimple-osx-x64.zip | tar xf -
-go build --tags fts5 -o gosimple
-./gosimple
+wget https://github.com/wangfenjin/simple/releases/download/v0.2.2/libsimple-linux-ubuntu-latest.zip && unzip libsimple-linux-ubuntu-latest.zip && rm -f libsimple-linux-ubuntu-latest.zip
+go run --tags fts5 .
